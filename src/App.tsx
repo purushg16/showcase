69a67b479120086cf1b817f809add0f222ce5b5f
Purushothaman G <officialpurushothaman@gmail.com>
2023-12-31 22:32:38 +0530

    fonts set-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {" "}
+      Art direction and digital support for Unibet quotes. Several digital
+      subjects including the creation of boosted ribs, videos with personalities
+      from each sport. Several graphic proposals and moods for future social
+      media activations.{" "}
+    </div>
+  );
 }
 
 export default App;
