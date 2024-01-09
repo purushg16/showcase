4476b570367d98081d5abfac6fb8e7cea16fa5dd
Purushothaman G <officialpurushothaman@gmail.com>
2024-01-09 17:53:08 +0530

    projects route created

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import Layout from "../pages/LayoutPage";
 import LandingPage from "../pages/Landing/LandingPage";
+import Projects from "../pages/Projects/Projects";
 
 const router = createBrowserRouter([
   {
@@ -10,7 +11,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <LandingPage /> },
-      //   { path: "/games/:slug", element: <GameDetailsPage /> },
+      { path: "/projects", element: <Projects /> },
     ],
   },
 ]);
