49bd60b731460ed9fc449b682e8d7be3a50f7b14
Purushothaman G <officialpurushothaman@gmail.com>
2024-01-23 09:23:38 +0530

    errorpage created

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Box, Flex, GridItem, Image, Text } from "@chakra-ui/react";
+import { IoHome } from "react-icons/io5";
+import { IconButton } from "../elements/Button";
+import { NavButton } from "../elements/Button";
+import AnimateMove from "../motions/Move";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <Flex
+      flexDirection="column"
+      w="100%"
+      h="100%"
+      alignItems="center"
+      justifyContent="center"
+      bg="#FAFAFA"
+    >
+      <Box textAlign="center">
+        <AnimateMove direction="y">
+          <Text fontSize="6xl" fontWeight={500}>
+            Oops, you got lost!
+          </Text>
+          <Text fontSize="lg" mb={5}>
+            Don't worry let's go home
+          </Text>
+          <IconButton
+            text="Let's Go Home"
+            route=""
+            left
+            icon={<IoHome />}
+            color="blue"
+          />
+        </AnimateMove>
+      </Box>
+    </Flex>
+  );
 };
 
 export default ErrorPage;
