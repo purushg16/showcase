946aede54efe564632e50b4b9ee50da0677a7c86
Purushothaman G <officialpurushothaman@gmail.com>
2024-01-29 18:45:46 +0530

    error page & background white solved

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,37 +1,30 @@
-import { Box, Flex, GridItem, Image, Text } from "@chakra-ui/react";
-import { IoHome } from "react-icons/io5";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { IoHomeSharp } from "react-icons/io5";
 import { IconButton } from "../elements/Button";
-import { NavButton } from "../elements/Button";
 import AnimateMove from "../motions/Move";
 
 const ErrorPage = () => {
   return (
-    <Flex
-      flexDirection="column"
-      w="100%"
-      h="100%"
-      alignItems="center"
-      justifyContent="center"
-      bg="#FAFAFA"
-    >
-      <Box textAlign="center">
-        <AnimateMove direction="y">
-          <Text fontSize="6xl" fontWeight={500}>
-            Oops, you got lost!
-          </Text>
-          <Text fontSize="lg" mb={5}>
-            Don't worry let's go home
-          </Text>
-          <IconButton
-            text="Let's Go Home"
-            route=""
-            left
-            icon={<IoHome />}
-            color="blue"
-          />
-        </AnimateMove>
+    <AnimateMove direction="y">
+      <Box position="absolute" h="100vh" w="100%">
+        <Flex
+          position="relative"
+          flexDirection="column"
+          w="100%"
+          h="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Box textAlign="center">
+            <Text fontSize="3xl" fontWeight={500}>
+              Oops, you got lost!
+            </Text>
+            <Text mb={5}>Don't worry let's go home</Text>
+            <IconButton text="Home" route="" left icon={<IoHomeSharp />} />
+          </Box>
+        </Flex>
       </Box>
-    </Flex>
+    </AnimateMove>
   );
 };
 
