77ccfec11f55a300a7ce813e4cf6d5792b10f638
Purushothaman G <officialpurushothaman@gmail.com>
2024-04-29 14:47:18 +0530

    contact form dark mode revised

--- src/components/elements/ContactForm.tsx
+++ src/components/elements/ContactForm.tsx
@@ -1,14 +1,25 @@
-import { Button, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  Show,
+  Text,
+  VStack,
+  useColorMode,
+} from "@chakra-ui/react";
 import { TbArrowUpRight } from "react-icons/tb";
 import { Link } from "react-router-dom";
 
 const ContactForm = () => {
+  const { colorMode } = useColorMode();
+
   return (
     <VStack
       textAlign="center"
-      p={{ base: 4, md: 8 }}
+      p={8}
       borderRadius={10}
-      bg="whiteAlpha.50"
+      bg={colorMode == "dark" ? "whiteAlpha.50" : "blackAlpha.50"}
+      border="1px solid"
+      borderColor={colorMode == "dark" ? "whiteAlpha.100" : "blackAlpha.100"}
     >
       <Heading fontSize="lg"> Contact Me </Heading>
       <Text fontSize="sm" mb={8}>
@@ -21,12 +32,15 @@ const ContactForm = () => {
           Macdasy
         </Link>
         .
-        <br />
+        <Show above="md">
+          <br />
+        </Show>{" "}
         By the way, I'm also looking for oppurtunities to work in a company or
         enterprise.
       </Text>
       <Link to="/connect">
         <Button
+          size={{ base: "sm", md: "md" }}
           bg="white"
           color="black"
           _hover={{ bg: "white", opacity: 0.8 }}
